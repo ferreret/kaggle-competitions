e592581fa1a83b7e7e2f551358eeda95c0d7a718
Nicolás Barceló <ferreret@gmail.com>
2023-10-11 16:46:16 +0200

    Titanic pruebas

--- imports.py
+++ imports.py
@@ -4,7 +4,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (accuracy_score, auc, classification_report,
-                             confusion_matrix, roc_auc_score, roc_curve)
-from sklearn.model_selection import GridSearchCV, train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+    accuracy_score,
+    auc,
+    classification_report,
+    confusion_matrix,
+    roc_curve,
+)
+from sklearn.model_selection import GridSearchCV, train_test_split, RandomizedSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+
+from sklearn.ensemble import RandomForestClassifier
